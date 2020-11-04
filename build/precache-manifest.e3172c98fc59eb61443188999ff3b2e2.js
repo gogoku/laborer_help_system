8a32ad85f4fd29a0c75ab888c19d472a30ee5efa
Gokul P <gokulp@Gokuls-MacBook-Pro.local>
2020-11-04 15:35:00 +0530

    new build

---
+++ build/precache-manifest.e3172c98fc59eb61443188999ff3b2e2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25477c95fe15a0dbe3d8048d6c239222",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94e8f2c51f0b51cc7068",
+    "url": "/static/css/main.698a66fd.chunk.css"
+  },
+  {
+    "revision": "d893f0f85da167d128da",
+    "url": "/static/js/2.da50ab08.chunk.js"
+  },
+  {
+    "revision": "7ffc4fdc219227d1429a83cbc7ac6d01",
+    "url": "/static/js/2.da50ab08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4bfdac80eee05b95bef",
+    "url": "/static/js/3.d8bf623f.chunk.js"
+  },
+  {
+    "revision": "9d460ab5a288ad8b2195",
+    "url": "/static/js/4.3ddacf64.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/static/js/4.3ddacf64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d49d7b6bcbf7067fb48d",
+    "url": "/static/js/5.c583ed61.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/static/js/5.c583ed61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94e8f2c51f0b51cc7068",
+    "url": "/static/js/main.cc4cb368.chunk.js"
+  },
+  {
+    "revision": "c28d2cdaa48b113ad47b",
+    "url": "/static/js/runtime-main.df93a640.js"
+  }
+]);
